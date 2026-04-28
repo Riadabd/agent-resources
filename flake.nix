3191d77b8703061dfc7741dfabc8402516c58081
Riad Abdallah <riad.abdallah@hotmail.com>
2026-04-28 23:45:07 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "agents-gen";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = { nixpkgs, zig, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      eachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              zig.packages.${system}."0.15.2"
+            ];
+          };
+        });
+    };
+}
